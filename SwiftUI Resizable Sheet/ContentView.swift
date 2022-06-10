4390f6a280beb3e4d68a752c06537e4f9b6a0dc4
Santiago Mattiauda <santimattius@gmail.com>
2022-06-10 20:50:29 -0300

    Initial Commit

---
+++ SwiftUI Resizable Sheet/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI Resizable Sheet
+//
+//  Created by Santiago Mattiauda on 10/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Resizable Sheet/SwiftUI_Resizable_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Resizable_SheetApp.swift
+//  SwiftUI Resizable Sheet
+//
+//  Created by Santiago Mattiauda on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Resizable_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
